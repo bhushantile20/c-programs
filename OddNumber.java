9bd01830aeb82e9a24ff6e43fa6d6490751aa2dc
Bhushan TIle <113130816+bhushantile20@users.noreply.github.com>
2024-01-02 23:37:44 +0530

    Add files via upload

---
+++ OddNumber.java
@@ -0,0 +1,14 @@
+class OddNumber
+{
+    public static void main(String args[])
+    {
+         System.out.println("The Odd Numbers are:");
+         for(int i=1;i<=100;i++)
+         {
+            if(i%2 !=0){
+            System.out.print(i+" ");
+
+         }
+    }
+}
+}
