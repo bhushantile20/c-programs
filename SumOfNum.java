6ab8a1873201b371c75e0c9c5de7e52f108d688c
Bhushan TIle <113130816+bhushantile20@users.noreply.github.com>
2024-01-09 19:27:10 +0530

    Create SumOfNum.java

---
+++ SumOfNum.java
@@ -0,0 +1,14 @@
+class SumOfNum
+{
+    public static void main(String args[])
+    {
+         int sum =0;
+         int n=10;
+         for(int i=1;i<=n;i++)
+         { 
+            sum=sum+i;
+         }
+         System.out.println("the Sum of "+n+"Numbers are:"+sum);
+         
+    }
+}
