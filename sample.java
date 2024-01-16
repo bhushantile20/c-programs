2a48688c108cfb6ff53f29f18d0ab1645f8e1824
Bhushan TIle <113130816+bhushantile20@users.noreply.github.com>
2024-01-16 22:46:44 +0530

    Add files via upload

---
+++ sample.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class sample
+{
+   public static void main(String args[]) 
+   {
+       ArrayList A1 = new ArrayList();
+       A1.add("Red");
+       A1.add(5);
+       A1.add("Null");
+       A1.add("Orange");
+       A1.add("Yellow");
+       System.out.println(A1);
+   }
+}
