ca0749ff9deb954c779b0bb74d99dbaf17d26130
Bhushan TIle <113130816+bhushantile20@users.noreply.github.com>
2024-01-18 23:18:02 +0530

    Add files via upload

---
+++ cookie2.php
@@ -0,0 +1,10 @@
+<?php
+$fstyle=$_COOKIE['fstyle'];
+$size=$_COOKIE['fsize'];
+$size=$_COOKIE['fcolor'];
+$color=$_COOKIE['bcolor'];
+
+echo"<body bgcolor=$bcolor>";
+echo"<font faces=$fstyle size=$fsize colors=$fcolor>CMCS
+</font>";
+?>
---
+++ student.php
@@ -0,0 +1,22 @@
+<?php
+    // //$_GET<$_POST,$_REQUEST
+
+    // echo "<br> COntent of get array <br>";
+
+    // print_r($_GET);
+
+    // echo"<br>Content of post arra<br>";
+    // print_r($_POST);
+
+  
+    // echo"<br>Content of post arra<br>";
+    // print_r($_REQUEST);
+
+    // echo "<br>ROll No :".$_REQURST['roll'];
+    // //15marks question inform 
+    // print_r($_SERVER);
+    //Advance for loop
+     foreach($_SERVER as $k=>$v)
+     echo"<br>".$k.":".$v
+//                                                                   o "<br> BROWSER name:".$_SERVER[HTTP_USER_AGENT]
+    ?>
