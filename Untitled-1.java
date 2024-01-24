923437762d5fd7e972e998bac1b69203525df76e
Bhushan TIle <113130816+bhushantile20@users.noreply.github.com>
2024-01-24 22:46:05 +0530

    Add files via upload

---
+++ Untitled-1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class dthread implements Runnable
+{
+     Thread t;
+     int i,no,sum;
+     dthread(String s,int n)
+     {
+         Random rs=new Random();
+         t=new Thread(this,s)
+           no=n;
+           int j=0;
+           for(i=1;i<=1000;i++)
+           {
+             a[j]
+           }
+     }
+}
