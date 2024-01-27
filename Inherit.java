2c79e08b7f11135bdca906f39b11d5f92b1f6610
Bhushan TIle <113130816+bhushantile20@users.noreply.github.com>
2024-01-27 23:54:56 +0530

    Add files via upload

---
+++ Inherit.java
@@ -0,0 +1,26 @@
+class A{
+    int a;
+    int b;
+    A()
+     {
+    System.out.prinln("class A executed");
+    }
+}
+class B extends A
+{
+    int c;
+    B(){ System.out.println("class b exectued");
+}
+}
+class Inherit
+{
+    public static void main(String args[]);
+    {
+    B b1=new B()
+
+    }}
+    
+//     System.out.prinln(a);
+//     System.out.prinln(b);
+//     System.out.prinln(c);
+// }
