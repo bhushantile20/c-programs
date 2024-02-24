f7a5626ea33da71680ae3dd48ea4b4914d6af744
Bhushan TIle <113130816+bhushantile20@users.noreply.github.com>
2024-02-24 23:08:10 +0530

    Add files via upload

---
+++ Manager.java
@@ -0,0 +1,43 @@
+import java.util.*; 
+class Employee 
+{ 
+ int BasicSalary=35000; 
+ public void getSalary() 
+ { 
+ System.out.println("Employee Salary:"+BasicSalary); 
+ } 
+} 
+class Manager extends Employee 
+{ 
+ int traveling =2000; 
+ int rent =5000; 
+ 
+ public void getSalary() 
+ { 
+ 
+ System.out.println("Manager Salary:"+(BasicSalary+traveling+rent)); 
+ System.out.println("Basic Salary:"+BasicSalary); 
+ System.out.println("Travaling Allowance:"+traveling); 
+ System.out.println("House rent:"+rent); 
+ 
+ } 
+ public static void main(String[] args) 
+ { 
+ Scanner sc=new Scanner(System.in); 
+ System.out.println("Press 1 for Employee Salary and 0 for Manager"); 
+ int ch=sc.nextInt(); 
+ if(ch==1) 
+ { 
+ Employee Eob=new Employee(); 
+ Eob.getSalary(); 
+ } 
+ else if(ch == 0) 
+ { 
+ Manager Mob=new Manager(); 
+ Mob.getSalary(); 
+ } 
+ else 
+ System.out.println("Entered Wrong Choice"); 
+ 
+ } 
+}
