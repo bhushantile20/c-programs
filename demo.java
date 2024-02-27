875994764efdb4df595e34e269883d776c8e9931
Bhushan TIle <113130816+bhushantile20@users.noreply.github.com>
2024-02-27 23:27:30 +0530

    Add files via upload

--- demo.java
+++ demo.java
@@ -1,30 +1,56 @@
-class demo
-{ 
-    public static void main(String args[])
-    {
-         int a[]={5,10};
-         int b=5;
-         try
-         {
-            int x=a[2]/b-a[1];
+//Slip16_2: Write a program to design a screen using Awt that,
+                           
+import javax.swing.*;
+import java.awt.*;
+import java.awt.font.*;
+import java.awt.event.*;
+class demo extends JFrame 
+{
+	JMenuBar mb;
+	JMenu m1,m2,m3;
+	JMenuItem open,save,cut,copy,new1,exit1;
+	JCheckBoxMenuItem jcm;
+	
+	demo()
+	{
+		setLayout(null);			
+		setSize(800,800);
+		setTitle("Login");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	
+		mb=new JMenuBar();
+		setJMenuBar(mb);
+		
+		m1=new JMenu("File");
+		mb.add(m1);	
+		
+		new1=new JMenuItem("New     CTRL+N");
+		m1.add(new1);
+		m1.setMnemonic(KeyEvent.VK_N);
+		open=new JMenuItem("Open");
+		m1.add(open);
+		
+		save=new JMenuItem("Save");
+		m1.add(save);
+		m1.addSeparator();
 
-         }
-         catch(ArithmeticException e)
-         {
-             System.out.println("Divison by zero");
+		jcm=new JCheckBoxMenuItem("Show About");
 
-         }
-         catch(ArrayIndexOutOfBoundsException e)
-         {
-             System.out.println("Array index error");
+		m1.add(jcm);
+		m1.addSeparator();
+		exit1=new JMenuItem("Exit");
+		m1.add(exit1);
 
-         }
-         catch(ArrayStoreException e)
-         {
-             System.out.println("Wrong data type");
-
-         }
-         int y=a[1]/a[0];
-         System.out.println("Y="+y);
-    }
+		m2=new JMenu("Edit");
+		mb.add(m2);
+		
+		m3=new JMenu("About");
+		mb.add(m3);
+		
+	}
+	public static void main(String args[]) 
+	{
+    	new demo();
+	}
 }
