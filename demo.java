dd54a32a5986a7c516ed472788cbda32b8cc4930
Bhushan TIle <113130816+bhushantile20@users.noreply.github.com>
2024-03-20 21:39:23 +0530

    Add files via upload

--- demo.java
+++ demo.java
@@ -1,56 +1,33 @@
-//Slip16_2: Write a program to design a screen using Awt that,
-                           
-import javax.swing.*;
-import java.awt.*;
-import java.awt.font.*;
-import java.awt.event.*;
-class demo extends JFrame 
-{
-	JMenuBar mb;
-	JMenu m1,m2,m3;
-	JMenuItem open,save,cut,copy,new1,exit1;
-	JCheckBoxMenuItem jcm;
-	
-	demo()
-	{
-		setLayout(null);			
-		setSize(800,800);
-		setTitle("Login");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setVisible(true);
-	
-		mb=new JMenuBar();
-		setJMenuBar(mb);
-		
-		m1=new JMenu("File");
-		mb.add(m1);	
-		
-		new1=new JMenuItem("New     CTRL+N");
-		m1.add(new1);
-		m1.setMnemonic(KeyEvent.VK_N);
-		open=new JMenuItem("Open");
-		m1.add(open);
-		
-		save=new JMenuItem("Save");
-		m1.add(save);
-		m1.addSeparator();
+//Slip19_2: Write a program which shows the combo box which includes list of T.Y.B.Sc.(Comp. Sci) subjects. Display the selected subject in a text field
 
-		jcm=new JCheckBoxMenuItem("Show About");
+import javax.swing.*; 
+import java.awt.event.*; 
+import java.awt.*;  
+public class demo extends JFrame implements ItemListener
+{    
+    JComboBox cb;
+    JTextField t1;
+    demo()
+    {   
+	    setLayout(new FlowLayout());   
+String sub[]={"TCS","JAVA","WT","FDS","OS"};
+        	cb=new    JComboBox(sub);    
 
-		m1.add(jcm);
-		m1.addSeparator();
-		exit1=new JMenuItem("Exit");
-		m1.add(exit1);
-
-		m2=new JMenu("Edit");
-		mb.add(m2);
-		
-		m3=new JMenu("About");
-		mb.add(m3);
-		
-	}
-	public static void main(String args[]) 
-	{
-    	new demo();
-	}
-}
+	add(cb);        
+           
+            setSize(400,500);    
+          
+	cb.addItemListener(this);
+	t1=new JTextField("selected subjects:"); 
+	add(t1);
+setVisible(true); 	         
+     }    
+     public void itemStateChanged(ItemEvent E)
+     {
+		t1.setText(cb.getSelectedItem() + " selected");
+     } 
+     public static void main(String[] args) 
+     {    
+       new demo();    
+     }    
+}  
