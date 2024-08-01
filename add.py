cf265256bcab283685a6c76c587260d2121f5e3c
Bhushan TIle <113130816+bhushantile20@users.noreply.github.com>
2024-08-01 23:05:20 +0530

    Add files via upload

---
+++ add.py
@@ -0,0 +1,12 @@
+num1=input("Enter first value")
+num2=input("Enter second value")
+print("numbers before swapping")
+print("num1=",num1)
+print("num2=",num2)
+
+temp=num1
+num1=num2
+num2=temp
+print("numbers after swapping")
+print("num1=" ,num1)
+print("num2=",num2)
